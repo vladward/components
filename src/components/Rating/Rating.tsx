711bd0e2e60a6d683f702fd482d10cca0a255c46
vladward <vladward13@gmail.com>
2021-10-25 13:44:57 +0300

    refactor: refactor onClick for Rating

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -6,27 +6,35 @@ type RatingPropsType = {
 
 type StarPropsType = {
     selected: boolean
+    value: 0 | 1 | 2 | 3 | 4 | 5
+    setValue: (value: 0 | 1 | 2 | 3 | 4 | 5) => void
 }
 
-export function Rating(props: RatingPropsType) {
 
+
+export function Rating(props: RatingPropsType) {
     const [value, setValue] = useState(0)
 
+    const clickValue = () => {
+        return (
+            setValue(value)
+        )
+    }
         return (
         <div>
-            <Star selected={value >= 1}/><button onClick={() => setValue(1)}>1</button>
-            <Star selected={value >= 2}/><button onClick={() => setValue(2)}>2</button>
-            <Star selected={value >= 3}/><button onClick={() => setValue(3)}>3</button>
-            <Star selected={value >= 4}/><button onClick={() => setValue(4)}>4</button>
-            <Star selected={value >= 5}/><button onClick={() => setValue(5)}>5</button>
+            <Star selected={value >= 1} setValue={setValue} value={1}/>
+            <Star selected={value >= 2} setValue={setValue} value={2}/>
+            <Star selected={value >= 3} setValue={setValue} value={3}/>
+            <Star selected={value >= 4} setValue={setValue} value={4}/>
+            <Star selected={value >= 5} setValue={setValue} value={5}/>
         </div>
         )
 }
 
 function Star(props: StarPropsType) {
-    if (props.selected) {
-        return <span><b>star</b> </span>
-    } else {
-        return <span>star </span>
-    }
+    return (
+            <span onClick={() => { props.setValue(props.value) }}>
+                {props.selected ? <b> star</b> : " star"}
+            </span>
+    )
 }
