fccddd505d053b8ac0fe78505d6e652dacf1baf9
vladward <vladward13@gmail.com>
2021-11-04 19:10:02 +0300

    feat: add input stories with ref

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -15,4 +15,20 @@ export const TrackValueOfUncontrolledInput = () => {
     return <> <input onChange={onChange}/> {value}</>
 }
 
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+    const [value, setValue] = useState('')
+    const inputRef = useRef<HTMLInputElement>(null)
+    const save = () => {
+        const el = inputRef.current as HTMLInputElement
+        setValue(el.value)
+    }
+    return <>
+        <input ref={inputRef} />
+        <button onClick={save}>
+            save
+        </button>
+        actual value - {value}
+    </>
+}
+
 export const ControlledInputWithFixedValue = () => <input value={'123'}/>
