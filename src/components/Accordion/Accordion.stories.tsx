fb313ceed3d7d4d933e0a6d93f668fed81e1efae
vladward <vladward13@gmail.com>
2021-11-11 13:31:13 +0300

    fix: accordion stories refactor

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -8,19 +8,38 @@ export default {
 }
 
 const callBack = action('accordion mode change event fired')
+const onClickCallBack = action('accordion item should be clicked')
 
 export const MenuCollapsedMode = () => <Accordion titleValue={"Menu"}
                                                   collapsed={false}
                                                   onChange={callBack}
+                                                  items={[]}
+                                                  onClick={()=>{}}
 />
+
 export const MenuUncollapsedMode = () => <Accordion titleValue={"Menu"}
                                                     collapsed={true}
                                                     onChange={callBack}
+                                                    items={[
+                                                        {title: 'Vlad', value: 1},
+                                                        {title: 'Andrei', value: 2},
+                                                        {title: 'Katya', value: 3},
+                                                        {title: 'Ira', value: 4}
+                                                    ]}
+                                                    onClick={onClickCallBack}
 />
+
 export const AccordionChanging = () => {
     const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
     return <Accordion titleValue={"Clickable Menu"}
                       collapsed={accordionCollapsed}
                       onChange={() => setAccordionCollapsed(!accordionCollapsed)}
+                      items={[
+                          {title: 'Vlad', value: 1},
+                          {title: 'Andrei', value: 2},
+                          {title: 'Katya', value: 3},
+                          {title: 'Ira', value: 4}
+                      ]}
+                      onClick={onClickCallBack}
     />
 }
