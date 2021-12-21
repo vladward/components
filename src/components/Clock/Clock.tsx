2c8502edf327c7f255931f021ef9f91e0c602be5
vladward <vladward13@gmail.com>
2021-12-21 15:39:32 +0300

    add correct date enable function

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,18 +1,20 @@
 import React, {useState} from "react"
 
 
-type PropsType = {
-
+const GetTime = (number: number) => {
+    return number < 10
+        ? '0' + number
+        : number
 }
 
 export const Clock: React.FC<PropsType> = (props) => {
     const [date, setDate] = useState<Date>(new Date)
 
     return <div>
-        <span>{date.getHours()}</span>
+        <span>{GetTime(date.getHours())}</span>
         :
-        <span>{date.getMinutes()}</span>
+        <span>{GetTime(date.getMinutes())}</span>
         :
-        <span>{date.getSeconds()}</span>
+        <span>{GetTime(date.getSeconds())}</span>
     </div>
 }
