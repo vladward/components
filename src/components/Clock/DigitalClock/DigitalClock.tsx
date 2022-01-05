484e3e9d5dc810d7a3e6aa5176dbc172e53dae48
vladward <vladward13@gmail.com>
2022-01-05 15:56:44 +0300

    feat: add margin for DigitalClock

--- src/components/Clock/DigitalClock/DigitalClock.tsx
+++ src/components/Clock/DigitalClock/DigitalClock.tsx
@@ -6,11 +6,11 @@ export const GetTime = (number: number) => {
 }
 
 export const DigitalClock: React.FC<ClockPropsType> = ({date}) => {
-    return <>
+    return <div style={{marginLeft: '30px'}}>
         <span>{GetTime(date.getHours())}</span>
         :
         <span>{GetTime(date.getMinutes())}</span>
         :
         <span>{GetTime(date.getSeconds())}</span>
-    </>
+    </div>
 }
