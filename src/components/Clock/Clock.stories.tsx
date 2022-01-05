5f63b700a4aa6ea4f5a6db94bbdbf24d51c0a580
vladward <vladward13@gmail.com>
2022-01-05 14:58:03 +0300

    ref: clock stories refactor

--- src/components/Clock/Clock.stories.tsx
+++ src/components/Clock/Clock.stories.tsx
@@ -1,10 +1,15 @@
+import React from 'react'
 import {Clock} from "./Clock";
 
 export default {
-    title: 'Clock',
+    title: 'Clock stories',
     component: Clock
 }
 
-export const BaseExample = () => {
-    return <Clock/>
+export const BaseAnalogExample = () => {
+    return <Clock mode={"analog"}/>
+}
+
+export const BaseDigitalExample = () => {
+    return <Clock mode={"digital"}/>
 }
