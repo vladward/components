e24e3b194f35a24ea07a627879d4b1c249072527
vladward <vladward13@gmail.com>
2022-01-05 14:58:41 +0300

    feat: add analog clock

---
+++ src/components/Clock/AnalogClock/AnalogClock.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {ClockPropsType} from "../Clock"
+import s from './AnalogClock.module.css'
+
+export const AnalogClock: React.FC<ClockPropsType> = ({date}) => {
+    const secondsStyle = {
+        transform: `rotate(${date.getSeconds() * 6}deg)`
+    };
+    const minutesStyle = {
+        transform: `rotate(${date.getMinutes() * 6}deg)`
+    };
+    const hoursStyle = {
+        transform: `rotate(${date.getHours() * 30}deg)`
+    };
+    return (
+        <div className={s.clock}>
+            <div className={s["analog-clock"]}>
+                <div className={`${s.dial} ${s.seconds}`} style={secondsStyle} />
+                <div className={`${s.dial} ${s.minutes}`} style={minutesStyle} />
+                <div className={`${s.dial} ${s.hours}`} style={hoursStyle} />
+            </div>
+    </div>
+    )
+}
